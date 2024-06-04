868522897d8ab5cafcaafa6d6f3ed46d21dc0fcc
William Venner <william@venner.io>
2024-06-04 03:57:33 +0100

    Documentation

--- src/node-bzip2.js
+++ src/node-bzip2.js
@@ -1,7 +1,44 @@
+/**
+ * NodeJS bindings for BZip2.
+ * @module node-bzip2
+ */
+
+/**
+* Configuration options for BZip2 compression.
+* @typedef {Object} CompressionOptions
+* @property {('auto'|'always'|'never')} [buffering] **`always`** **(default)** is somewhat slower but reallocates memory as needed (and therefore uses significantly less memory)
+*
+* **`never`** may be faster but requires an allocation roughly the same size as the input data
+*
+* **`auto`** will only reallocate memory as needed if the input is larger than 1 GiB.
+* @property {Number} [level] Compression level from `1` to `9` **(default: `9`)**
+*/
+
+/**
+* Configuration options for BZip2 decompression.
+* @typedef {Object} DecompressionOptions
+* @property {Boolean} [small] If **`true`**, the decompressor will use less memory but be slower **(default: `false`)**
+*/
+
 const bindings = require('bindings');
 const nodeBzip2 = bindings('node_bzip2');
 
-module.exports = {
-    compress: nodeBzip2.compress,
-	decompress: nodeBzip2.decompress
-};
+/**
+ * Compresses the given data using BZip2.
+ * @param {Buffer|string|Uint8Array|Uint8ClampedArray|Int16Array|Uint16Array|Int32Array|Uint32Array} data The data to be compressed.
+ * @param {CompressionOptions} [options] Configuration for the compression.
+ * @returns {Buffer} The compressed data.
+ */
+export function compress(data, options) {
+	return nodeBzip2.compress(data, options);
+}
+
+/**
+ * Decompresses the given data using Bzip2.
+ * @param {Buffer} data The data to be decompressed.
+ * @param {DecompressionOptions} [options] Configuration for the decompression.
+ * @returns {Buffer} The decompressed data.
+ */
+export function decompress(data, options) {
+	return nodeBzip2.decompress(data, options);
+}
